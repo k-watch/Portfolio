d839474c73ad2ba26f7385d9e9e1e44248ba19b5
k-watch <kwonyong6@gmail.com>
2020-12-14 20:52:06 +0900

    gradle react 연동 및 빌드 성공 최초 ver

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getApi();
+  }
+
+  getApi = () => {
+    axios
+      .get("http://localhost:8080/api/hello")
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          message: res.data.message,
+        });
+      })
+      .catch((res) => console.log(res));
+  };
+
+  render() {
+    return <div> Main 페이지{`${this.state.message}가나다`}</div>;
+  }
+}
+
+export default App;
